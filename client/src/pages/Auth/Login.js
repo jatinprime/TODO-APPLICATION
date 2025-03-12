447724d65630455484648185c018f92ad181da7e
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-12 17:06:07 +0530

    add login page with form styling and navigation to registration

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./AuthStyles.css";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
+    return (
+        <div className="form-container">
+            <div className="form">
+                <div className="mb-3">
+                    <i className="fa-solid fa-circle-user"></i>
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="enter email"
+                    />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="enter password"
+                    />
+                </div>
+                <div className="form-bottom">
+                    <p className="text-center">
+                        Not a user? please
+                        <Link to="/register"> Register</Link>
+                    </p>
+                    <button type="submit" className="login-btn">
+                        LOGIN
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
