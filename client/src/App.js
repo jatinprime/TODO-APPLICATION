becd47558e81187f1125db10b2c133b8fc093790
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-12 13:01:04 +0530

    implement routing functionality with react-router-dom in App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import About from "./pages/About/About";
+import TodoList from "./pages/ToDos/TodoList";
 
+//Routes will work like a container
+//Route will be a single working route
+
+/*------------------------------
+TO APPLY ROUTING FUNCTIONALITY
+
+1) npm i react-router-dom
+2) Inside index.js , import Browser Router and wrap the container inside it
+3) In App.js , import Routes and Route , and apply the following path according with the element
+-------------------------------*/
 
 function App() {
     return (
         <div>
-            <h1>Welcome to this website</h1>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/todoList" element={<TodoList />} />
+            </Routes>
         </div>
     );
 }
