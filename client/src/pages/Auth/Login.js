aa1381cd35f9bff0964bc1f1ddf6ee514daf90fc
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-12 17:27:19 +0530

    implement login and registration functionality with form handling and state management

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./AuthStyles.css";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    //login function
+    const loginHandler = (e) => {
+        try {
+            e.preventDefault(); //to avoid the default behaviour
+            alert("Login Data " + email + " " + password);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className="form-container">
             <div className="form">
@@ -14,6 +27,10 @@ const Login = () => {
                         type="email"
                         className="form-control"
                         placeholder="enter email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-3">
@@ -21,6 +38,10 @@ const Login = () => {
                         type="password"
                         className="form-control"
                         placeholder="enter password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="form-bottom">
@@ -28,7 +49,7 @@ const Login = () => {
                         Not a user? please
                         <Link to="/register"> Register</Link>
                     </p>
-                    <button type="submit" className="login-btn">
+                    <button type="submit" className="login-btn" onClick={loginHandler}>
                         LOGIN
                     </button>
                 </div>
--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./AuthStyles.css";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username , setUsername] = useState("") ;
 
-export default Register
+    //login function
+    const registerHandler = (e) => {
+        try {
+            e.preventDefault(); //to avoid the default behaviour
+            alert("Register Data " + email + " " + password + " " + username);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="form-container">
+            <div className="form">
+                <div className="mb-3">
+                    <i className="fa-solid fa-circle-user"></i>
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="enter user name"
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="enter email"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="mb-3">
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="enter password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="form-bottom">
+                    <p className="text-center">
+                        Already user? please
+                        <Link to="/login"> Login</Link>
+                    </p>
+                    <button type="submit" className="login-btn" onClick={registerHandler}>
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
