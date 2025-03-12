68bd58795b49bffb2491464556cd7fa3b1f70966
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-12 13:37:07 +0530

    add landing page styles and hero image for improved visual presentation

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import todo from '../../assets/images/todo.jpg'
+import "./Landing.css"
 
 const Landing = () => {
     return (
@@ -7,6 +9,10 @@ const Landing = () => {
             <div className="intro-text">
                 <h1>
                     <span className="tagline1">Organize work and life</span>
+                    <br />
+                    <span className="tagline2">
+                        Finally
+                    </span>
                 </h1>
                 <p>
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
@@ -18,6 +24,9 @@ const Landing = () => {
                 <Link className="btn blue" to="/login">
                     Login
                 </Link>
+            </div>
+            <div className="">
+                <img src={todo} alt="heroimage" width={"100%"} height={515} />
             </div>
         </div>
     );
