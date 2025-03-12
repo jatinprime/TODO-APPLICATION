e67ff0bfdb77f46ae2ab341eeb97c699cb8cd33e
jatinprime <jatinagrawal28112003@gmail.com>
2025-03-12 13:12:57 +0530

    enhance landing page with introductory text and navigation links for registration and login

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  )
-}
+    return (
+        <div className="hero">
+            <div className="intro-text">
+                <h1>
+                    <span className="tagline1">Organize work and life</span>
+                </h1>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Similique, ex.
+                </p>
+                <Link className="btn red" to="/register">
+                    Register Now!
+                </Link>
+                <Link className="btn blue" to="/login">
+                    Login
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Landing
+export default Landing;
